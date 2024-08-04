aee76920408d0aba889a5c77b1638178be05425f
chepeniv <jose.olmos@atlasschool.com>
2024-08-04 08:43:32 -0500

    [failing] node pointer logic

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,10 +1,5 @@
 #include "sort.h"
 
-/*
- * the value stored within a node cannot be changed
- * only the nodes themselves can be moved around
- */
-
 /**
  * insertion_sort_list -
  * @list: pointer holding the location of the first node in a list
@@ -15,22 +10,53 @@ void insertion_sort_list(listnode **first)
 {
 	listnode **end, **pos, **current; /*malloc these*/
 
-	*end = *first;
+	/*
+	int length = 0;
+	while ((*first)->next != NULL)
+		length++;
+	if (length < 3)
+		return;
+	*/
 
+	end = malloc(sizeof(void *));
+	pos = malloc(sizeof(void *));
+	current = malloc(sizeof(void *));
+	if (end == NULL || pos == NULL || current == NULL)
+		return;
+
+	printf("entered first whileloop\n");
+	*end = *first;
 	while ((*end)->next != NULL)
 	{
 		*pos = *end;
 		*current = (*end)->next;
 		while ((*pos)->n > (*current)->n)
 		{
-			if ((*pos)->prev != NULL)
-				*pos = (*pos)->prev;
-			else
+			*pos = (*pos)->prev;
+			if (*pos == NULL)
 				break;
 		}
+
 		if (*pos != *end)
-			/* insert current before pos) */;
+		{
+			if (*pos != NULL)
+			{
+				if ((*current)->next != NULL)
+					(*current)->next->prev = (*current)->prev;
+				(*current)->prev->next = (*current)->next;
+				(*current)->prev = *pos;
+				(*current)->next = (*pos)->next;
+				(*pos)->next = *current;
+				(*current)->next->prev = *current;
+			}
+			print_list(*first);
+		}
+
 		if ((*end)->n < (*end)->next->n)
 			*end = (*end)->next;
 	}
+
+	free(end);
+	free(pos);
+	free(current);
 }
