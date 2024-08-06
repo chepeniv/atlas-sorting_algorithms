1198a2700b88863aac56c66608b2230bf4f29840
chepeniv <jose.olmos@atlasschool.com>
2024-08-05 22:49:54 -0500

    [passing] removed printfs

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -9,7 +9,7 @@
  */
 void quick_sort(int *array, size_t len)
 {
-	size_t pivot = len - 1, part = 0 , curr = 0 , next = 0 ;
+	size_t pivot = len - 1, curr = 0 , next = 0 ;
 	int hold;
 
 	while (curr <= pivot)
@@ -24,8 +24,6 @@ void quick_sort(int *array, size_t len)
 					hold = array[curr];
 					array[curr] = array[next];
 					array[next] = hold;
-					part++;
-					printf("part(%d)\n", array[part]);
 					print_array(array, len);
 					break;
 				}
@@ -36,16 +34,13 @@ void quick_sort(int *array, size_t len)
 				hold = array[pivot];
 				array[pivot] = array[curr];
 				array[curr] = hold;
-				part++;
 				print_array(array, len);
-				printf("part(%d)\n", array[part]);
 			}
 		}
 		if (curr == pivot)
 		{
 			pivot = pivot - 1;
 			curr = 0;
-			printf("pivot(%d), part(%d)\n", array[pivot], array[part]);
 		}
 		++curr;
 	}
